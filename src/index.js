8b88f2687dd914f83403e63e60dddd9b71b31082
Nikita <nikitavoznyuk37@gmail.com>
2020-04-06 20:05:34 +0300

    Refactor state.js

--- src/index.js
+++ src/index.js
@@ -1,8 +1,22 @@
 import * as serviceWorker from './serviceWorker';
-import state from './redux/state';
-import { rerenderEntireTree } from './render';
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+import App from './App';
+import store from './redux/state';
+import { BrowserRouter } from 'react-router-dom';
 
-rerenderEntireTree(state);
+let rerenderEntireTree = (state) => {
+  ReactDOM.render(
+    <BrowserRouter>
+      <App updatePostTitle={store.updatePostTitle.bind(store)} state={state} addPost={store.addPost.bind(store)} />
+    </BrowserRouter>,
+    document.getElementById('root')
+  );
+}
+
+rerenderEntireTree(store.getState());
+store.subscribe(rerenderEntireTree);
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
--- src/redux/state.js
+++ src/redux/state.js
@@ -1,62 +1,69 @@
-import { rerenderEntireTree } from '../render';
+let store = {
+  _state: {
+    profile: {
+      postsData: [
+        {
+          id: 1,
+          src: 'https://profilepicture7.com/img/img_dongman/2/1075497004.jpg',
+          title: 'About my weekend',
+          likes: 17
+        },
+        {
+          id: 2,
+          src: 'https://profilepicture7.com/img/img_dongman/2/1075497004.jpg',
+          title: 'Some picture with my crush',
+          likes: '31'
+        }
+      ],
+      newPostTitle: ''
+    },
+    messages: {
+      dialogsData: [
+          { id: 1, name: 'Kyong' },
+          { id: 2, name: 'Daeun' },
+          { id: 3, name: 'Eunji' },
+          { id: 4, name: 'Jiyoung' },
+          { id: 5, name: 'Sunyoung' }
+      ],
+      messagesData: [
+        { id: 1, text: 'Hello' },
+        { id: 2, text: 'How are you?' },
+        { id: 3, text: 'What is your favorite pet?' }
+      ]
+    },
+    sidebar: {
+      friends: [
+        { id: 1, img: 'https://avatarfiles.alphacoders.com/822/82242.png', name: 'Makoto' },
+        { id: 2, img: 'https://www.elsetge.cat/myimg/f/90-907317_couple-wallpaper-matching-icons-avatar-couple-couple-glitch.jpg', name: 'Rina' },
+        { id: 3, img: 'https://profilepicture7.com/img/img_dongman/2/938383067.jpg', name: 'Tsuki' }
+      ]
+    }
+  },
+  getState() {
+    return this._state;
+  },
+  _rerenderEntireTree() {
 
-let state = {
-  profile: {
-    postsData: [
-      {
-        id: 1,
-        src: 'https://profilepicture7.com/img/img_dongman/2/1075497004.jpg',
-        title: 'About my weekend',
-        likes: 17
-      },
-      {
-        id: 2,
-        src: 'https://profilepicture7.com/img/img_dongman/2/1075497004.jpg',
-        title: 'Some picture with my crush',
-        likes: '31'
-      }
-    ],
-    newPostTitle: ''
   },
-  messages: {
-    dialogsData: [
-        { id: 1, name: 'Kyong' },
-        { id: 2, name: 'Daeun' },
-        { id: 3, name: 'Eunji' },
-        { id: 4, name: 'Jiyoung' },
-        { id: 5, name: 'Sunyoung' }
-    ],
-    messagesData: [
-      { id: 1, text: 'Hello' },
-      { id: 2, text: 'How are you?' },
-      { id: 3, text: 'What is your favorite pet?' }
-    ]
+  addPost(postTitle) {
+    let post = {
+      id: 3,
+      src: 'https://profilepicture7.com/img/img_dongman/2/1075497004.jpg',
+      title: this._state.profile.newPostTitle,
+      likes: 0
+    };
+
+    this._state.profile.postsData.push(post);
+    this._state.profile.newPostTitle = '';
+    this._rerenderEntireTree(this._state);
   },
-  sidebar: {
-    friends: [
-      { id: 1, img: 'https://avatarfiles.alphacoders.com/822/82242.png', name: 'Makoto' },
-      { id: 2, img: 'https://www.elsetge.cat/myimg/f/90-907317_couple-wallpaper-matching-icons-avatar-couple-couple-glitch.jpg', name: 'Rina' },
-      { id: 3, img: 'https://profilepicture7.com/img/img_dongman/2/938383067.jpg', name: 'Tsuki' }
-    ]
+  updatePostTitle(text) {
+    this._state.profile.newPostTitle = text;
+    this._rerenderEntireTree(this._state);
+  },
+  subscribe(observer) {
+    this._rerenderEntireTree = observer;
   }
 }
 
-export let addPost = (postTitle) => {
-  let post = {
-    id: 3,
-    src: 'https://profilepicture7.com/img/img_dongman/2/1075497004.jpg',
-    title: state.profile.newPostTitle,
-    likes: 0
-  };
-
-  state.profile.postsData.push(post);
-  state.profile.newPostTitle = '';
-  rerenderEntireTree(state);
-}
-
-export let updatePostTitle = (text) => {
-  state.profile.newPostTitle = text;
-  rerenderEntireTree(state);
-}
-
-export default state;
+export default store;
