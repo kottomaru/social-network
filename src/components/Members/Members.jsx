1a02aa0b6a7494903eeadfe84b0ae4192bb7c3f1
Nikita <nikitavoznyuk37@gmail.com>
2020-04-11 15:16:05 +0300

    Refactor Members functional component to class component

--- src/components/Members/Members.jsx
+++ src/components/Members/Members.jsx
@@ -2,44 +2,48 @@ import React from 'react';
 import styles from './Members.module.css';
 import * as axios from 'axios';
 
-let Members = (props) => {
+class Members extends React.Component {
 
-  let getMembersFromAPI = () => {
-    if (props.members.length === 3) {
+  getMembersFromAPI = () => {
+    if (this.props.members.length === 3) {
       axios
         .get('https://social-network.samuraijs.com/api/1.0/users')
         .then(res => {
-        props.setMembers(res.data.items);
+        this.props.setMembers(res.data.items);
       });
     }
   }
 
-  return (
-    <div>
-      {
-        props.members.map((member) => <div key={member.id} className={styles.container}>
-            <div className={styles.containerPhoto}>
-              <img src={member.avatarUrl} className={styles.avatar} />
-              { member.follow
-                ? <button onClick={ () => { props.unfollow(member.id) } }>Unfollow</button>
-                : <button onClick={ () => { props.follow(member.id) } }>Follow</button>}
-            </div>
-            <div className={styles.memberInfo}>
-              <div className={styles.memberName}>
-                {member.name}
+  render() {
+    return (
+      <div>
+        {
+          this.props.members.map((member) => <div key={member.id} className={styles.container}>
+              <div className={styles.containerPhoto}>
+                <img src={member.avatarUrl} className={styles.avatar} />
+                { member.follow
+                  ? <button onClick={ () => { this.props.unfollow(member.id) } }>Unfollow</button>
+                  : <button onClick={ () => { this.props.follow(member.id) } }>Follow</button>}
               </div>
-              <div>
-                <div>{"member.location.city"}</div>
-                <div>{"member.location.country"}</div>
+              <div className={styles.memberInfo}>
+                <div className={styles.memberName}>
+                  {member.name}
+                </div>
+                <div>
+                  <div>{"member.location.city"}</div>
+                  <div>{"member.location.country"}</div>
+                </div>
               </div>
-            </div>
-        </div>)
-      }
-      { props.members.length < 4
-        ? <button onClick={getMembersFromAPI}>Show More</button>
-        : <div>all members displaying</div>}
-    </div>
-  );
+          </div>)
+        }
+        <div className={styles.showContainer}>
+        { this.props.members.length < 4
+          ? <button onClick={this.getMembersFromAPI}>Show More</button>
+          : <div>all members displaying</div>}
+        </div>
+      </div>
+    );
+  }
 };
 
 export default Members;
